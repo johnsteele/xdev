da21dcfdab2044e16e09b93596a4a4abab5a935f
John Steele <programjsteele@gmail.com>
2013-05-19 13:59:30 -0500

    setup

--- xdev-remotespace/com.steelejr.remotespace.runtime.workbench/src/com/steelejr/remotespace/runtime/workbench/E4Application.java
+++
@@ -1,6 +0,0 @@
-package com.steelejr.remotespace.runtime.workbench;
-
-import org.eclipse.equinox.app.IApplication;
-import org.eclipse.equinox.app.IApplicationContext;
-
-public class E4Application extends 
