b01e3466a130730168ffaa8566ca181144e9b516
John Steele <programjsteele@gmail.com>
2013-06-10 12:40:07 -0700

    setup

---
+++ tester/src/tester/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package tester;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "tester.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
---
+++ tester3/src/tester3/ICommandIds.java
@@ -0,0 +1,15 @@
+package tester3;
+
+/**
+ * Interface defining the application's command IDs.
+ * Key bindings can be defined for specific commands.
+ * To associate an action with a command, use IAction.setActionDefinitionId(commandId).
+ *
+ * @see org.eclipse.jface.action.IAction#setActionDefinitionId(String)
+ */
+public interface ICommandIds {
+
+    public static final String CMD_OPEN = "tester3.open";
+    public static final String CMD_OPEN_MESSAGE = "tester3.openMessage";
+    
+}
