fd1db89699fd94c7530567fc66bccde71c5296c5
John Steele <programjsteele@gmail.com>
2013-06-13 10:44:00 -0700

    setup

---
+++ com.steelejr.xdev.fx.app/src/com/steelejr/xdev/fx/app/view/Editor2.java
@@ -0,0 +1,39 @@
+ 
+package com.steelejr.xdev.fx.app.view;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+
+public class Editor2 {
+	@Inject
+	public Editor2() {
+		//TODO Your code here
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		//TODO Your code here
+	}
+	
+	
+	@PreDestroy
+	public void preDestroy() {
+		//TODO Your code here
+	}
+	
+	
+	@Focus
+	public void onFocus() {
+		//TODO Your code here
+	}
+	
+	
+	@Persist
+	public void save() {
+		//TODO Your code here
+	}
+	
+}
